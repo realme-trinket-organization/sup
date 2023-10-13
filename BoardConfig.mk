6fbb172a2e93a3da6378f0094904ace090313ea3
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-10-14 01:50:58 +0700

    realme_trinket: Define architecture flags
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,13 @@
 
 DEVICE_PATH := device/realme/realme_trinket
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo300
+
 # Inherit the proprietary files
 include vendor/realme/realme_trinket/BoardConfigVendor.mk
