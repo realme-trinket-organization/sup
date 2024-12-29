a35c8b89f27bb2417818e1faa1e10f659fcc60c6
dodyirawan85 <dev.irawans@gmail.com>
2024-12-29 16:58:16 +0700

    realme_trinket: Import rootdir from `RMX1911EX_11.C.83_1830_202211301527`
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,36 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.oppo.wifi.sh \
+    init.qcom.class_core.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh
+
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab-wlan.qcom \
+    init.msm.usb.configfs.rc \
+    init.oppo.debug.diag.rc \
+    init.oppo.reserve.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    init.wlan.qcom.rc \
+    init.wlan.target.rc \
+    ueventd.qcom.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
